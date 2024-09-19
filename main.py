0559a8d407e1058c9d559f8d101607bf2a11f4eb
Toms-T <toms.teicis@valmiera.edu.lv>
2024-09-19 15:04:11 +0300

    (izveidot mašīnas klases)

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("sveika pasaule")
+class Mašīnas:
+    def __init__(self,markaaaaa,modelis,krāsa):
+        self.marka=markaaaaa
+        self.modelis=modelis
+        self.krasa=krāsa
+    def braukt(self):
+        print(self)
+
+m1=Mašīnas("BMW","E36","Melna")
+print(m1.krasa)
+
+ 
